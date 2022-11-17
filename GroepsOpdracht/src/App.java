454419a93664427f542fb1ca64bb4abbf4772e5f
Luuk Beks <beksluuk@gmail.com>
2022-11-17 10:28:53 +0100

    contributor test

--- GroepsOpdracht/src/App.java
+++ GroepsOpdracht/src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("tuan");
-        System.out.println("Luuk");
+        System.out.println("Luuk Beks");
         System.out.println("Stef");
         System.out.println("Miquel");
     }
