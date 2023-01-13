67bfe16d7e4a7435b63bfe82b1ffda21b4ef4dc6
kgithcoy <anhtuannguyen.2004@gmail.com>
2023-01-13 22:12:33 +0100

    logic package toegevoegd met tests

---
+++ codecademy2/src/main/java/com/codecademy2/Logic/Logic.java
@@ -0,0 +1,76 @@
+package com.codecademy2.Logic;
+public class Logic {
+    
+    public boolean dateTool(int day, int month, int year)
+    {
+        if(day>1 && day<=31  && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12))
+        {
+            return true;
+        }
+        else if ((month == 4 || month == 6 || month == 9 || month == 11) && day>1 && day<=30)
+        {
+            return true;
+        }
+        else if (month == 2 && day>1 && day<=29 && (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)))
+        {
+            return true;
+        }
+        else if (month == 2 && day>1 && day<=28 && (year % 4 != 0 || (year % 100 == 0 && year % 400 != 0)))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public boolean mailTool(String mailAddress)
+    {
+        //Explain of the regex key
+        //^	The beginning of a line
+        //[] A group of
+        //\\ disables in line code like \n
+        //w-_.+ any letter or number and dash, underscore, dot and plus only once
+        //* zero or more after this
+        //() as a capturing group
+        //$	The end of a line
+        String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]*[\\w]$";
+        // return outcome determined whether email matches the regex key
+        return mailAddress.matches(regex);
+    }
+    public boolean numericRangeTools(int percentage)
+    {
+        if(percentage>=0 && percentage<=100)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public String postalCode(String postalCode)
+    {
+        if(postalCode == null)
+        {
+            throw new NullPointerException();
+        }
+        else if (Integer.valueOf(postalCode.trim().substring(0, 4)) > 999 &&
+                    Integer.valueOf(postalCode.trim().substring(0, 4)) <= 9999 &&
+                    postalCode.trim().substring(4).trim().length() == 2 &&
+                    'A' <= postalCode.trim().substring(4).trim().toUpperCase().charAt(0) &&
+                    postalCode.trim().substring(4).trim().toUpperCase().charAt(0) <= 'Z' &&
+                    'A' <= postalCode.trim().substring(4).trim().toUpperCase().charAt(0) &&
+                    postalCode.trim().substring(4).trim().toUpperCase().charAt(0) <= 'Z') 
+            {
+                String result = postalCode.trim().substring(0, 4) + " " +
+                        postalCode.trim().substring(4).trim().toUpperCase();
+                return result;
+            }
+            else {
+            throw new IllegalArgumentException();
+    }
+    
+}
+}
+
---
+++ codecademy2/src/main/java/com/codecademy2/Logic/LogicTest.java
@@ -0,0 +1,200 @@
+
+package com.codecademy2.Logic;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+import org.junit.Test;
+
+public class LogicTest {
+    @Test
+    public void testDateFebInLeapYearOutOfRange(){
+        //arrange
+        Logic validator= new Logic();
+
+        //act
+       boolean valid = validator.dateTool(30, 2, 2022);
+
+        //assert
+        assertEquals(false, valid);
+    }
+     @Test
+    public void testDateFebInLeapYearInRange(){
+        //arrange
+        Logic validator= new Logic();
+
+        //act
+       boolean valid = validator.dateTool(12, 2, 2022);
+
+        //assert
+        assertEquals(true, valid);
+    }
+    @Test
+    public void testDateNovInRange(){
+        //arrange
+        Logic validator= new Logic();
+
+        //act
+       boolean valid = validator.dateTool(23, 11, 2022);
+
+        //assert
+        assertEquals(true, valid);
+    }
+   
+    @Test
+    public void testDateAprOutOfRange(){
+        //arrange
+        Logic validator= new Logic();
+
+        //act
+       boolean valid = validator.dateTool(31, 4, 2006);
+
+        //assert
+        assertEquals(false, valid);
+    }
+
+    @Test
+    public void validateMailAddressReturnsFalseNoAt(){
+        //arrange
+        Logic validator = new Logic();
+        String mailAddress = "atnguyen.com";
+        //act
+        Boolean result = validator.mailTool(mailAddress);
+        //assert
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void validateMailAddressReturnsFalseNoMailbox(){
+        //arrange
+        Logic validator = new Logic();
+        String mailAddress = "@mail.com";
+        //act
+        Boolean result = validator.mailTool(mailAddress);
+        //assert
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void validateMailAddressReturnsTrueNoSubdomainTLDDelimiter(){
+        //arrange
+        Logic validator = new Logic();
+        String mailAddress = "mail@mail.hfgs.com";
+        //act
+        Boolean result = validator.mailTool(mailAddress);
+        //assert
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void validateMailAddressReturnsFalseNoSubdomain(){
+        //arrange
+        Logic validator = new Logic();
+        String mailAddress = "mail@.com";
+        //act
+        Boolean result = validator.mailTool(mailAddress);
+        //assert
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void validateMailAddressReturnsFalseNoTLD(){
+        //arrange
+        Logic validator = new Logic();
+        String mailAddress = "mail@mail.";
+        //act
+        Boolean result = validator.mailTool(mailAddress);
+        //assert
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void validateMailAddressReturnsTrue(){
+        //arrange
+        Logic validator = new Logic();
+        String mailAddress = "at.nguyen2@mail.com";
+        //act
+        Boolean result = validator.mailTool(mailAddress);
+        //assert
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void testIsValidPercentageReturnsTrue(){
+        //arrange
+        Logic validator = new Logic();
+        int percentage = 50;
+        //act
+        Boolean result = validator.numericRangeTools(percentage);
+        //assert
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void testIsValidPercentageReturnsFalseBelowZero(){
+        //arrange
+        Logic validator = new Logic();
+        int percentage = -1;
+        //act
+        Boolean result = validator.numericRangeTools(percentage);
+        //assert
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void testIsValidPercentageReturnsFalseAboveOneHundred(){
+        //arrange
+        Logic validator = new Logic();
+        int percentage = 110;
+        //act
+        Boolean result = validator.numericRangeTools(percentage);
+        //assert
+        assertEquals(false, result);
+    }
+
+
+    @Test
+    public void nullPointerExceptionPostalCode (){
+         //arrange
+         Logic validator = new Logic();
+
+         //act
+        
+ 
+         //assert
+         assertThrows(NullPointerException.class, () -> validator.postalCode(null));
+        
+    }
+    @Test
+    public void notEnoughNumbers (){
+        //arrange
+        Logic validator = new Logic();
+
+        //act
+       
+
+        //assert
+        assertThrows(IllegalArgumentException.class, () -> validator.postalCode("123A"));
+   }
+   @Test
+   public void rightInput (){
+     //arrange
+     Logic validator = new Logic();
+
+     //act
+    String valid = validator.postalCode("1234ab");
+
+     //assert
+     assertEquals("1234 AB", valid);
+}
+    @Test
+    public void differentInput (){
+     //arrange
+     Logic validator = new Logic();
+
+     //act
+
+     //assert
+     assertThrows(IllegalArgumentException.class, () -> validator.postalCode("test"));
+}
+}
