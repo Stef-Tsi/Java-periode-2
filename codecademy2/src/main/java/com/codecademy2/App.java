5dcc6fae89a9ac27c108a2d0c8ada8f5dd19248f
luukbeks <beksluuk@gmail.com>
2023-01-13 15:55:50 +0100

    changed the dbconnection, it returns true or false

--- codecademy2/src/main/java/com/codecademy2/App.java
+++ codecademy2/src/main/java/com/codecademy2/App.java
@@ -1,8 +1,6 @@
 package com.codecademy2;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -19,20 +17,18 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        stage.setTitle("Codecademy");
+
+        DbConnection db = new DbConnection();
+        if (db.makeConnection()) {
+            System.out.println(" Connection made");
+        } else {
+            System.out.println(" Connection failed");
+        }
+
         stage.setScene(scene);
         stage.show();
-        DbConnection db = new DbConnection();
-        db.makeConnection();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+  
     }
 
     public static void main(String[] args) {
--- codecademy2/src/main/java/com/codecademy2/DB/DbConnection.java
+++ codecademy2/src/main/java/com/codecademy2/DB/DbConnection.java
@@ -7,14 +7,34 @@ import java.sql.SQLException;
 
 
 public class DbConnection {
+    private String url;
+    private String user;
+    private String password;
 
-    public void makeConnection() {
-        String url = "jdbc:sqlserver://aei-sql2.avans.nl:1443;databaseName=LuukIsTheBest;";
-        String user = "LuukB";
-        String password = "Welkom!123";
+    public DbConnection() {
+        this.url = "jdbc:sqlserver://aei-sql2.avans.nl:1443;databaseName=LuukIsTheBest;";
+        this.user = "LuukB";
+        this.password = "Welkom!123";
+    }
+
+    public boolean makeConnection() {
         try (Connection db = DriverManager.getConnection(url, user, password)) {
                                                             //SUSER_SNAME() returns the current user
             PreparedStatement query = db.prepareStatement("SELECT SUSER_SNAME();");
+            ResultSet result = query.executeQuery();
+            while (result.next()) {
+                System.out.printf(result.getString(1));
+            }
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public void getAllCertificates() {
+        try (Connection db = DriverManager.getConnection(url, user, password)) {
+            PreparedStatement query = db.prepareStatement("SELECT * FROM Certificate;");
             ResultSet result = query.executeQuery();
             while (result.next()) {
                 System.out.printf(result.getString(1));
@@ -24,7 +44,8 @@ public class DbConnection {
         } finally {
             System.out.println("\n Done");
         }
+
     }
-    
+
 
 }
